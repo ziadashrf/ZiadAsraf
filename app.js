8c71d06ecdabd7ed4bdedb89c4cad61734ea5e31
ziadashrf <65822988+ziadashrf@users.noreply.github.com>
2020-11-13 21:09:41 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let theInput = (document.getElementById("input").oninput = function () {
+  let theResult = Math.floor((this.value / 14) * 10);
+  console.log(typeof theResult);
+  let ch = String.fromCharCode(this.value);
+  if (isNaN(this.value)) {
+    document.getElementById("result").textContent = ` يسطا اكتب رقم بلاش غباء`;
+  } else {
+    document.getElementById(
+      "result"
+    ).textContent = `هيوصلك ${theResult}  جنيه `;
+  }
+  if (this.value >= 50000000) {
+    document.getElementById("result").textContent = ` بطل لعب يا حبيبي`;
+  }
+});
