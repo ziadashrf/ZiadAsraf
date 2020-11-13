266d821740e329514b7244cecd69e29a3dbef38d
ziadashrf <65822988+ziadashrf@users.noreply.github.com>
2020-11-13 22:30:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 let theInput = (document.getElementById("input").oninput = function () {
   let theResult = Math.floor((this.value / 14) * 10);
+  console.log(typeof theResult);
+  let result = document.getElementById("result");
   if (isNaN(this.value)) {
-    document.getElementById("result").textContent = ` يسطا اكتب رقم بلاش غباء`;
+    result.textContent = ` يسطا اكتب رقم بلاش غباء`;
   } else {
-    document.getElementById(
-      "result"
-    ).textContent = `هيوصلك ${theResult}  جنيه `;
+    result.textContent = `هيوصلك ${theResult}  جنيه `;
   }
   if (this.value >= 50000000) {
-    document.getElementById("result").textContent = ` بطل لعب يا حبيبي`;
+    result.textContent = ` بطل لعب يا حبيبي`;
   }
   if (this.value < 0) {
-    document.getElementById(
-      "result"
-    ).textContent = ` ازي بقي انا عاوز حضرتك تفهني ازي`;
+    result.textContent = ` ازي بقي انا عاوز حضرتك تفهني ازي`;
   }
 });
